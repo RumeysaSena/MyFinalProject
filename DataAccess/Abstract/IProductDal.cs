cb0c30591b5e319f2ca7af189dcf53fa13f490fb
Rumeysa Sena Özdemir <77540535+ozrumsena@users.noreply.github.com>
2021-02-12 22:30:51 +0300

    8.Gün

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -6,13 +6,8 @@ using Entities.Concrete;
 namespace DataAccess.Abstract
 {
     // IProductDal - Productla ilgili veritabanında yapacağımız operasyonları (ekle, sil, güncelle gibi ) içeren interfacedir. 
-    public interface IProductDal
+    public interface IProductDal : IEntityRepository<Product>
     {
-        List<Product> GetAll();
-        void Add(Product product);
-        void UpDate(Product product);
-        void Delete(Product product);
-
-        List<Product> GetAllByCategory(int categoryId); // // ürünleri kategoriye göre filtrele.
+       // // ürünleri kategoriye göre filtrele.
     }
 }
